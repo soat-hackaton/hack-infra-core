2c83a8a047750dd636cc7f401c379d9a603fffff
Philipp Hahmann <55932959+philipphahmann@users.noreply.github.com>
2026-02-01 19:53:34 -0300

    ajustado nomes dos recursos para evitar conflito de duplicidade

--- envs/dev/variables.tf
+++ envs/dev/variables.tf
@@ -1,19 +1,19 @@
 variable "s3_bucket_name" {
   description = "Nome do Bucket para vídeos (deve ser único globalmente)"
   type        = string
-  default     = "uploaded-videos"
+  default     = "hack-uploaded-videos"
 }
 
 variable "dynamo_table_name" {
   description = "Nome da tabela de metadados"
   type        = string
-  default     = "video-processing-db"
+  default     = "hack-video-processing-db"
 }
 
 variable "sqs_queue_name" {
   description = "Nome da fila SQS"
   type        = string
-  default     = "video-processing-queue"
+  default     = "hack-video-processing-queue"
 }
 
 variable "aws_region" {
